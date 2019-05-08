beb5dd2698aecd4980b28a9f2f649415b35c8928
Sergio Rosa <scarlosjazz@gmail.com>
2019-05-08 12:04:59 -0400

    DefaultProps

--- gonative1/src/components/Todo.js
+++ gonative1/src/components/Todo.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 export default class Todo extends Component {
+  static defaultProps = {
+    title: "Excercise"
+  };
   render() {
     return (
       <View>
