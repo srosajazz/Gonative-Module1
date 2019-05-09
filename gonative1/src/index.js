04b57384e472d2fecac930e2a13f28f1c2ec12d9
Sergio Rosa <scarlosjazz@gmail.com>
2019-05-09 11:30:02 -0400

    React Native Component Lifecycle Api

--- gonative1/src/index.js
+++ gonative1/src/index.js
@@ -1,32 +1,38 @@
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View, todos } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
 
-import Todo from "./components/Todo";
+//import Todo from "./components/Todo";
 
 //const todos = ["Make Caffe", "study React Native"];
 
 export default class App extends Component {
   state = {
-    user: "Sergio",
-    todos: [{ id: 0, text: "Make Caffe" }, { id: 1, text: "Jazz" }]
+    counter: 0
   };
 
-  addTodo = () => {
-    this.setState({
-      todos: [...this.state.todos, { id: Math.random(), text: "New todo" }]
-    });
+  componentDidMount() {
+    // setTimeout(() => {
+    //   this.setState({ text: "Hello Sergio" });
+    // }, 3000);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextState.counter < 5;
+  }
+
+  handleAddCounter = () => {
+    this.setState({ counter: this.state.counter + 1 });
   };
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    return { text: nextProps.text };
+  }
 
   render() {
     return (
-      <View>
-        <View style={styles.container} />
-        <Text>{this.state.user}</Text>
-        {this.state.todos.map(todo => (
-          <Todo key={todo.id} title={todo.text} />
-        ))}
-        {/* <Button title="Add todo" onPress={this.addTodo} /> */}
-        {/* <Todo title="Make caffe" /> */}
+      <View style={styles.container}>
+        <Button onPress={this.handleAddCounter} title="Add" />
+        <Text>{this.state.counter}</Text>
       </View>
     );
   }
